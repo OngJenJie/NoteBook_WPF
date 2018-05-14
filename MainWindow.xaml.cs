c6afb75e6d469596ee96c85f35dd00093bbcaa17
CancerNeko <jenjieong@yahoo.com>
2018-05-14 19:30:03 +0800

    加了拉動title可以移動的程序碼

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -138,5 +138,13 @@ namespace BasicWpfNotepad
         {
             this.Close();
         }
+
+        private void TitleBg_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
     }
 }
